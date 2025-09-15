8955e045105a740e234d5d6a129345ce75537052
SJ <shivanshjha93@gmail.com>
2025-09-15 17:42:06 +0530

    hi

--- Regression/What model to choose/polynomial_reg.py
+++ Regression/What model to choose/polynomial_reg.py
@@ -21,4 +21,4 @@ np.set_printoptions(precision=2)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
 from sklearn.metrics import r2_score
-r2_score(y_test, y_pred)
+print(r2_score(y_test, y_pred))
